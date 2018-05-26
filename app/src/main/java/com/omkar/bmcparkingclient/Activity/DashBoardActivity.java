989db126fbb7063dded2f5b08cea19314902ef25
Yogesh <Yogesh2591@gmail.com>
2018-05-26 16:02:52 +0530

    commit

--- app/src/main/java/com/omkar/bmcparkingclient/Activity/DashBoardActivity.java
+++ app/src/main/java/com/omkar/bmcparkingclient/Activity/DashBoardActivity.java
@@ -72,7 +72,6 @@ public class DashBoardActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_read) {
